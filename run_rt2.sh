08e9f7cd9865affcb6e87b40deb13490a89cc4bd
Tim Semmelhaack <semmelhaack@zvbn.de>
2024-11-15 15:37:41 +0100

    Verfeinuerung Ausgabe Matrix

--- run_rt2.sh
+++ run_rt2.sh
@@ -4,4 +4,5 @@
 source /home/zvbn/python/rt2/.venv/bin/activate
 #/home/zvbn/python/rt2/.venv/bin/jupyter nbconvert --to notebook --execute /home/zvbn/python/rt2/rt_api2_prod_duckdb.ipynb --allow-errors
 /home/zvbn/python/rt2/.venv/bin/jupyter nbconvert --output-dir='/home/zvbn/python/rt2/log' --to notebook --execute /home/zvbn/python/rt2/rt_api_import.ipynb --allow-errors
+/home/zvbn/python/rt2/.venv/bin/jupyter nbconvert --output-dir='/home/zvbn/python/rt2/log' --to notebook --execute /home/zvbn/python/rt2/rt_api_import_matrix.ipynb --allow-errors
 /home/zvbn/python/rt2/.venv/bin/jupyter nbconvert --output-dir='/home/zvbn/python/rt2/log' --to notebook --execute /home/zvbn/python/rt2/rt_auswertung_parquet.ipynb --allow-errors
